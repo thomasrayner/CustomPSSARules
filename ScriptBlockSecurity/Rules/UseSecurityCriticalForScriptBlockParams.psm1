7762d67f7ade3974169ac940c3673e9b498d84d8
Thomas Rayner <thmsrynr@outlook.com>
2017-05-01 13:22:46 -0600

    added some inline comments to identify closing braces

--- ScriptBlockSecurity/Rules/UseSecurityCriticalForScriptBlockParams.psm1
+++ ScriptBlockSecurity/Rules/UseSecurityCriticalForScriptBlockParams.psm1
@@ -11,8 +11,7 @@ function Test-ScriptBlockParamSecurity {
         try {
             if ( $ScriptBlockAst.ParamBlock ) {
 		    	$paramBlocks = ($ScriptBlockAst.FindAll( { $args[0] -is [System.Management.Automation.Language.ParamBlockAst] }, $true )).Parameters | Select-Object -Unique
-                foreach ( $paramBlock in $paramBlocks )
-                {
+                foreach ( $paramBlock in $paramBlocks ) {
                     if ( $paramBlock.StaticType.Name -contains 'ScriptBlock' ) {
                         $sbParams = $paramBlock | Where-Object { $_.StaticType.Name -eq 'ScriptBlock' }
                         foreach ( $sbParam in $sbParams ) {
@@ -22,15 +21,15 @@ function Test-ScriptBlockParamSecurity {
 		    			            Extent   = $sbParam.Extent
 		    			            RuleName = 'PSUseSecurityCriticalForScriptBlockParams'
 		    			            Severity = 'Warning'
-		    		            }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+		    		            } # [PSCustomObject]
+                            } # if $sbParam.Attributes.TypeName.FullName -notcontains 'Security.SecurityCritical'
+                        } # foreach $sbParam in $sbParams
+                    } # if $paramblock.StaticType.Name -contains 'ScriptBlock'
+                } # foreach $paramblock in $paramBlocks
+            } # if $ScriptBlockAst.ParamBlock
+        } # try
         catch {
             $PSCmdlet.ThrowTerminatingError( $_ )
-        }
-    }
-}
+        } # catch
+    } # process
+} # function Test-ScriptBlockParamSecurity
