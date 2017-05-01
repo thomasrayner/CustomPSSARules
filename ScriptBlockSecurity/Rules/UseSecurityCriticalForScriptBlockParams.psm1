9b3b61a41cba114d2acbb3c3cec8c1023ba8bc44
Thomas Rayner <thmsrynr@outlook.com>
2017-05-01 16:01:15 -0600

    cleanup

--- ScriptBlockSecurity/Rules/UseSecurityCriticalForScriptBlockParams.psm1
+++ ScriptBlockSecurity/Rules/UseSecurityCriticalForScriptBlockParams.psm1
@@ -10,9 +10,9 @@ function Test-ScriptBlockParamSecurity {
     process {
         try {
             if ( $ScriptBlockAst.ParamBlock ) {
-		    	$paramBlocks = ($ScriptBlockAst.FindAll( { $args[0] -is [System.Management.Automation.Language.ParamBlockAst] }, $true ))
+		    	$paramBlocks = $ScriptBlockAst.FindAll( { $args[0] -is [System.Management.Automation.Language.ParamBlockAst] }, $true )
                 foreach ( $paramBlock in $paramBlocks ) {
-                    if ( ($paramBlock.Parameters.StaticType.Name -contains 'ScriptBlock') -and ($paramBlock.Attributes.TypeName.FullName -notcontains 'Security.SecurityCritical' ) ) {
+                    if ( ( $paramBlock.Parameters.StaticType.Name -contains 'ScriptBlock' ) -and ( $paramBlock.Attributes.TypeName.FullName -notcontains 'Security.SecurityCritical' ) ) {
                         
                         [PSCustomObject]@{
 		    			    Message  = 'Avoid using parameters that accept a scriptblock without specifying [Security.SecurityCritical()]'
